690dbe7a22656244ed4099064c0b1fb3836d1c1b
jmtrn <64854174+jmtrn@users.noreply.github.com>
2020-06-14 17:52:51 +0200

    jeu...

---
+++ jeu.h
@@ -0,0 +1,20 @@
+#ifndef JEUHEADER
+#define JEUHEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "annonces.h"
+
+
+
+
+void cardsTrump(int trump,CARD T[],int *nbtrump);
+
+
+
+
+
+
+
+#endif // JEUHEADER
